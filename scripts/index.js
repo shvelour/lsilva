d3a30cb0a3e2d6865d2b798fa00790b3d65caf6b
Jessica <jb229507@gmail.com>
2024-09-23 23:11:23 -0300

    alteracoes

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,18 @@ window.addEventListener('scroll', function () {
   if (this.window.pageYOffset > 0) return navbar.classList.add('active');
   return navbar.classList.remove('active');
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+});
